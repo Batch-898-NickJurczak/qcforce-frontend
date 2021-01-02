b428635bb2394473435a791e8db742a90c6ecb6f
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-02 14:38:11 -0500

    Worked on ngIf reguarding service response

--- src/app/takeSurvey/take-survey.component.spec.ts
+++ src/app/takeSurvey/take-survey.component.spec.ts
@@ -1,22 +1,37 @@
+import { HtmlParser } from '@angular/compiler';
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { TakeSurveyService } from '../services/take-survey.service';
+import { SurveyFormComponent } from './survey-form/survey-form.component';
 
 import { TakeSurveyComponent } from './take-survey.component';
 
-class MockRoute{
-  params : { [key : string]:string } = { token : `48n613x938nm384n2b`};
-  queryParams = of(this.params); 
+class MockRoute {
+  params: { [key: string]: string } = { token: `48n613x938nm384n2b` };
+  queryParams = of(this.params);
 }
+
+class MockService {
+  getSurveyForm(token: string): any {
+    return ['success', null];
+  }
+}
+
 describe('TakeSurveyComponent', () => {
   let component: TakeSurveyComponent;
   let fixture: ComponentFixture<TakeSurveyComponent>;
+  let HTMLElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: [ {provide: ActivatedRoute, useClass: MockRoute}]
-    })
-    .compileComponents();
+      providers: [
+        { provide: ActivatedRoute, useClass: MockRoute },
+        { provide: TakeSurveyService, useClass: MockService }
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -28,9 +43,21 @@ describe('TakeSurveyComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  
-  it ( 'should initialize token', async(() =>{
-    expect (component.token).toEqual(`48n613x938nm384n2b`);
+
+  it('should initialize token', async(() => {
+    expect(component.token).toEqual(`48n613x938nm384n2b`);
   }));
 
+  it('should display app-survey-form', () => {
+    fixture.detectChanges();
+    const printOut = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(printOut.textContent).toEqual('take-survey success!');
+  });
+
+  it('should not display app-survey-form', () => {
+    component.status = 'failure';
+    fixture.detectChanges();
+    const printOut = fixture.debugElement.query(By.css('span')).nativeElement;
+    expect(printOut.textContent).toEqual('take-survey works!');
+  });
 });
--- src/app/takeSurvey/take-survey.component.ts
+++ src/app/takeSurvey/take-survey.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SurveyForm } from '../models/survey-form.model';
+import { TakeSurveyService } from '../services/take-survey.service';
 
 @Component({
   selector: 'app-take-survey',
   templateUrl: './take-survey.component.html',
-  styleUrls: ['./take-survey.component.css']
+  styleUrls: ['./take-survey.component.css'],
 })
 export class TakeSurveyComponent implements OnInit {
-  
-  token !:String;
-  constructor( private route: ActivatedRoute,) { }
+  token!: string;
+  status: string;
+  survey: SurveyForm;
+  displaySurvey = false;
+
+  constructor(private route: ActivatedRoute, private service: TakeSurveyService) {}
 
   ngOnInit(): void {
-      /*  takes in token from the url PATH ' survey?token=948n613x938nm384n2b'  */ 
-      this.route.queryParams.subscribe(params => { 
-        this.token=params["token"];
-        })
-      
+    /*  takes in token from the url PATH ' survey?token=948n613x938nm384n2b'  */
+    this.route.queryParams.subscribe((params) => {
+      this.token = params.token;
+    });
+
+    this.setSurvey();
   }
-  
+
+  setSurvey(): any {
+    const response = this.service.getSurveyForm(this.token);
+    this.status = response[0];
+    this.survey = response[1];
+  }
 }
