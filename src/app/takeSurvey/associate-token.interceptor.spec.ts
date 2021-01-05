55c6bc1dfab64e530c7c26d1a4c4b755d2d957ae
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 10:52:03 -0500

    cleaned up code formating

--- src/app/takeSurvey/associate-token.interceptor.spec.ts
+++ src/app/takeSurvey/associate-token.interceptor.spec.ts
@@ -7,48 +7,47 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('AssociateTokenInterceptor', () => {
+  let store: MockStore;
+  let service: TakeSurveyService;
+  let httpMock: HttpTestingController;
 
-    let store: MockStore;
-    let service: TakeSurveyService;
-    let httpMock: HttpTestingController;
+  const initialState: AssociateSurvey = {
+    survey: null,
+    loaded: false,
+    loading: false,
+    token: 'yugaa738279ns25vbns0mdm093n9m',
+    error: '',
+  };
 
-    const initialState: AssociateSurvey = {
-        survey: null,
-        loaded: false,
-        loading: false,
-        token: 'yugaa738279ns25vbns0mdm093n9m',
-        error: ''
-    };
-
-    beforeEach(async(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-        providers:
-            [ provideMockStore({initialState}),
-                TakeSurveyService,
-                {provide: HTTP_INTERCEPTORS, useClass: AssociateTokenInterceptor, multi: true}
-            ],
+      imports: [HttpClientTestingModule],
+      providers: [
+        provideMockStore({ initialState }),
+        TakeSurveyService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AssociateTokenInterceptor,
+            multi: true
+        },
+      ],
     }).compileComponents();
-    }));
+  }));
 
-    beforeEach(() => {
-        store = TestBed.inject(MockStore);
-        service = TestBed.inject(TakeSurveyService);
-        httpMock = TestBed.inject(HttpTestingController);
+  beforeEach(() => {
+    store = TestBed.inject(MockStore);
+    service = TestBed.inject(TakeSurveyService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
+  it('should add the token into the HTTP header', () => {
+    service.getSurveyForm('').subscribe((response) => {
+      expect(response).toBeTruthy();
     });
-    afterEach( () => {
-        httpMock.verify();
-    });
-
-    it ('should add the token into the HTTP header', () => {
-        service.getSurveyForm('').subscribe(res => {
-            expect(res).toBeTruthy();
-        });
-        const httpReq = httpMock.expectOne(`http://survey/`);
-        expect(httpReq.request.headers.has(`Authorization`)).toContain(`Bearer ${initialState.token}`);
-    });
-
+    const httpReq = httpMock.expectOne(`http://survey/`);
+    expect(httpReq.request.headers.has(`Authorization`)).toContain(`Bearer ${initialState.token}`);
+  });
 });
-
-
