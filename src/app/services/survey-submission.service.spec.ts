81b53f71a2049338079d6517da98f215ec87b16f
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 13:52:23 -0500

    Fixed broken tests

--- src/app/services/survey-submission.service.spec.ts
+++ src/app/services/survey-submission.service.spec.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { SurveySubmissionService } from './survey-submission.service';
+
+class MockHttp{
+
+}
 
 describe('SurveySubmissionService', () => {
   let service: SurveySubmissionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ { provide: HttpClient , useClass: MockHttp } ]
+    });
     service = TestBed.inject(SurveySubmissionService);
   });
 
--- src/app/takeSurvey/take-survey.component.spec.ts
+++ src/app/takeSurvey/take-survey.component.spec.ts
@@ -3,6 +3,7 @@ import { ChangeDetectorRef, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { of } from 'rxjs';
 import { TakeSurveyService } from '../services/take-survey.service';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
@@ -21,17 +22,31 @@ class MockService {
 }
 
 describe('TakeSurveyComponent', () => {
+  let initialState;
+  let store;
   let component: TakeSurveyComponent;
   let fixture: ComponentFixture<TakeSurveyComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       providers: [
+        provideMockStore({ initialState }),
         { provide: ActivatedRoute, useClass: MockRoute },
         { provide: TakeSurveyService, useClass: MockService }
       ],
       declarations: [TakeSurveyComponent]
     }).compileComponents();
+    store = TestBed.inject(MockStore);
+    initialState  = {
+      associateSurvey: {
+        survey: null,
+        loaded: false,
+        loading: false,
+        token: 'yugaa738279ns25vbns0mdm093n9m',
+        error: '',
+      },
+    };
+    store.setState(initialState);
   }));
 
   beforeEach(() => {
