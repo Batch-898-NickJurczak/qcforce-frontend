0243add758da3366c115f1807ab30e3550f4481b
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 13:36:01 -0500

    Added addition test for if token is empty

--- src/app/takeSurvey/associate-token.interceptor.spec.ts
+++ src/app/takeSurvey/associate-token.interceptor.spec.ts
@@ -2,20 +2,13 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { environment } from 'src/environments/environment';
+import { SurveysService } from '../services/surveys.service';
 import { TakeSurveyService } from '../services/take-survey.service';
 import { AssociateTokenInterceptor } from './associate-token.interceptor';
 
 describe('Associate Token Interceptor', () => {
-  const initialState = {
-    associateSurvey: {
-      survey: null,
-      loaded: false,
-      loading: false,
-      token: 'yugaa738279ns25vbns0mdm093n9m',
-      error: '',
-    },
-  };
-
+  let initialState;
   let store: MockStore;
   let service: TakeSurveyService;
   let httpMock: HttpTestingController;
@@ -36,6 +29,16 @@ describe('Associate Token Interceptor', () => {
     store = TestBed.inject(MockStore);
     service = TestBed.inject(TakeSurveyService);
     httpMock = TestBed.inject(HttpTestingController);
+    initialState  = {
+        associateSurvey: {
+          survey: null,
+          loaded: false,
+          loading: false,
+          token: 'yugaa738279ns25vbns0mdm093n9m',
+          error: '',
+        },
+      };
+    store.setState(initialState);
   });
 
   it('service should be created for tests', () => {
@@ -43,17 +46,36 @@ describe('Associate Token Interceptor', () => {
   });
 
   it('should add the token into the HTTP header', fakeAsync(() => {
-    store.setState(initialState);
+
+    // Make http request with service
     service.getSurveyForm('').subscribe((response) => {
       expect(response).toBeTruthy();
     });
 
+    // Simulate an asynchronous call
     tick();
+
+    // Check request and ensure header has auth set with token
     const httpReq = httpMock.expectOne(`survey/`);
     expect(httpReq.request.headers.get(`Authorization`)).toBe(`Bearer ${initialState.associateSurvey.token}`);
   }));
 
-  // TODO: Add test to check if token is added to requests outside of the Take Survey Module (it shouldn't)
+  it('should not try to add the token to requests if the token state is null', fakeAsync(() => {
 
-  // TODO: Add test to make sure that if token is null, does not try to add to request
+    // Set token to empty
+    initialState.associateSurvey.token = '';
+    store.setState(initialState);
+
+    // Make http request with service
+    service.getSurveyForm('').subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    // Simulate an asynchronous call
+    tick();
+
+    // Check request and ensure header does not have auth set
+    const httpReq = httpMock.expectOne(`survey/`);
+    expect(httpReq.request.headers.has(`Authorization`)).toEqual(false);
+  }));
 });
