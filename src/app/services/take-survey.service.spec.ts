d57c3eee8bab3575965057d1e3270c92a9256730
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 17:12:54 -0500

    Updated tests and comments

--- src/app/services/take-survey.service.spec.ts
+++ src/app/services/take-survey.service.spec.ts
@@ -1,25 +1,59 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
-
-
+import { SurveySubmission } from '../models/survey-submission.model';
 import { TakeSurveyService } from './take-survey.service';
 
-class MockHttp{
-
+/**
+ *  Set up mock HttpClient to handle a get request returning format expected
+ */
+class MockHttp {
+  public get(URL: string): Observable<HttpResponse<any>> {
+    const response: HttpResponse<any> = new HttpResponse({ body: [`message`, null] });
+    return of(response);
+  }
 }
 
 describe('TakeSurveyService', () => {
   let service: TakeSurveyService;
+  let surveySubmission: SurveySubmission;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ { provide: HttpClient , useClass: MockHttp } ]
+      providers: [{ provide: HttpClient, useClass: MockHttp }],
     });
     service = TestBed.inject(TakeSurveyService);
+
+    // Set up survey object for testing
+    surveySubmission = {
+      id: 0,
+      surveyId: 0,
+      createdOn: new Date(Date.now()),
+      employeeId: 0,
+      batchId: 0,
+      answers: ['I like fries', 'I like all potatoes'],
+    };
   });
 
+  /**
+   * Test that component builds properly
+   */
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  /**
+   * Test that service is unpacking payload from body
+   * and not modifying the data within.
+   */
+  it('should return a list with a string message and an object null', () => {
+
+    // Unpack returned subscribable
+    let returned;
+    service.getSurveyForm('').subscribe((response) => (returned = response));
+
+    // Verify content is as expected
+    expect(returned[0]).toEqual(`message`);
+    expect(returned[1]).toEqual(null);
   });
 });
