b7f3ca3e120ee898bc416e9d4289af06e3269a08
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 13:01:22 -0500

    Implemented interceptor

--- src/app/takeSurvey/associate-token.interceptor.ts
+++ src/app/takeSurvey/associate-token.interceptor.ts
@@ -1,19 +1,30 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from '../store';
 
 @Injectable()
 export class AssociateTokenInterceptor implements HttpInterceptor{
 
-    constructor( private store: AppState){}
+    private token$ = this.store.pipe(select(state => state.associateSurvey.token));
+
+    constructor(private store: Store<AppState>){}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        request = request.clone ({
-            setHeaders: {
-                Authorization: `Bearer ${this.store.associateSurvey.token}`
-            }
-        });
+
+        this.token$.subscribe( token =>
+            console.log(token)
+        );
+
+        this.token$.subscribe(token =>
+            request = request.clone ({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+        );
+
         return next.handle(request);
     }
 }
