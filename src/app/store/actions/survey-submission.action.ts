7bf13b4fc246593838136f8bb8b63fd31891ef16
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 18:33:30 -0500

    Updated actions to use constants

--- src/app/store/actions/survey-submission.action.ts
+++ src/app/store/actions/survey-submission.action.ts
@@ -1,25 +1,35 @@
 import { createAction, props } from '@ngrx/store';
 import { SurveySubmission } from 'src/app/models/survey-submission.model';
 
-export const surveyReset = createAction(
-  '[Take Survey] Survey Reset'
+/**
+ * Subset of actions related to loading of a survey for an associate
+ * to fill out and the name of the module where the action will dispatched;
+ */
+export const SUBMISSION_RESET = '[Take Survey] Submission Reset';
+export const SUBMISSION_UPDATE = '[Take Survey] Submission Update';
+export const SUBMISSION_SUBMIT = '[Take Survey] Submission Submit';
+export const SUBMISSION_SUCCESS = '[Take Survey] Submission Success';
+export const SUBMISSION_FAILURE = '[Take Survey] Submission Failure';
+
+export const submissionReset = createAction(
+  SUBMISSION_RESET
 );
 
-export const surveyUpdate = createAction(
-  '[Take Survey] Survey Update',
+export const submissionUpdate = createAction(
+  SUBMISSION_UPDATE,
   props<{submission: SurveySubmission}>()
 );
 
-export const surveySubmit = createAction(
-  '[Take Survey] Survey Submit',
+export const submissionSubmit = createAction(
+  SUBMISSION_SUBMIT,
   props<{submission: SurveySubmission}>()
 );
 
 export const submissionSuccess = createAction(
-  '[Take Survey] Submission Success'
+  SUBMISSION_SUCCESS
 );
 
 export const submissionFailure = createAction(
-  '[Take Survey] Submission Failure',
+  SUBMISSION_FAILURE,
   props<{error: any}>()
 );
