fb065cf0c7a769f66eb649b70fa4f88f89f194ca
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-05 13:01:10 -0500

    Implemented test for interceptor

--- src/app/takeSurvey/associate-token.interceptor.spec.ts
+++ src/app/takeSurvey/associate-token.interceptor.spec.ts
@@ -1,53 +1,59 @@
-import { async, ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
-import { AssociateTokenInterceptor } from './associate-token.interceptor';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { AssociateSurvey } from '../store';
-import { TakeSurveyService } from '../services/take-survey.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { TakeSurveyService } from '../services/take-survey.service';
+import { AssociateTokenInterceptor } from './associate-token.interceptor';
 
-describe('AssociateTokenInterceptor', () => {
+describe('Associate Token Interceptor', () => {
+  const initialState = {
+    associateSurvey: {
+      survey: null,
+      loaded: false,
+      loading: false,
+      token: 'yugaa738279ns25vbns0mdm093n9m',
+      error: '',
+    },
+  };
+
   let store: MockStore;
   let service: TakeSurveyService;
   let httpMock: HttpTestingController;
 
-  const initialState: AssociateSurvey = {
-    survey: null,
-    loaded: false,
-    loading: false,
-    token: 'yugaa738279ns25vbns0mdm093n9m',
-    error: '',
-  };
-
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
         provideMockStore({ initialState }),
-        TakeSurveyService,
         {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AssociateTokenInterceptor,
-            multi: true
+          provide: HTTP_INTERCEPTORS,
+          useClass: AssociateTokenInterceptor,
+          multi: true,
         },
+        { provide: TakeSurveyService, useClass: TakeSurveyService },
       ],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
+    });
     store = TestBed.inject(MockStore);
     service = TestBed.inject(TakeSurveyService);
     httpMock = TestBed.inject(HttpTestingController);
   });
-  afterEach(() => {
-    httpMock.verify();
+
+  it('service should be created for tests', () => {
+    expect(service).toBeTruthy();
   });
 
-  it('should add the token into the HTTP header', () => {
+  it('should add the token into the HTTP header', fakeAsync(() => {
+    store.setState(initialState);
     service.getSurveyForm('').subscribe((response) => {
       expect(response).toBeTruthy();
     });
-    const httpReq = httpMock.expectOne(`http://survey/`);
-    expect(httpReq.request.headers.has(`Authorization`)).toContain(`Bearer ${initialState.token}`);
-  });
+
+    tick();
+    const httpReq = httpMock.expectOne(`survey/`);
+    expect(httpReq.request.headers.get(`Authorization`)).toBe(`Bearer ${initialState.associateSurvey.token}`);
+  }));
+
+  // TODO: Add test to check if token is added to requests outside of the Take Survey Module (it shouldn't)
+
+  // TODO: Add test to make sure that if token is null, does not try to add to request
 });
